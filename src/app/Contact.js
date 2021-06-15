ecd4f5eeec0fe17162eb97cb8369538f6e52c56e
piotr <peter.zyskowski@gmail.com>
2021-06-15 22:45:47 +0200

    email form added

--- src/app/Contact.js
+++ src/app/Contact.js
@@ -14,20 +14,18 @@ export class Contact{
     pressedEmailButton(button){
         button.classList.add("button--pressed");
         const horizontalFrame = document.querySelector(".contact__horizontal-frame");
+        const contact = document.querySelector(".contact")
         const frame = document.querySelector(".contact__overflow")
         horizontalFrame.classList.add("contact__horizontal-frame--active")
 
         setTimeout(() =>{
             frame.classList.add("contact__overflow--active");
-        },600)
-        
-        
+            contact.style.zIndex = "1";            
+        },1000)
     };
 
     loadFunctionality(){
         this.emailButtonFunctionality();
     }
-
-
 };
 
